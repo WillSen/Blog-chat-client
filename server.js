9e329d4c7e24b726590c51f032982548d142353c
Will Sentance <william.sentance@gmail.com>
2015-01-13 17:55:18 -0800

    server port set by env or 3000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require(‘express’),
     server = require(‘http’).Server(app),
     io = require(‘socket.io’)(server)
 
-server.listen(3000); 
+server.listen(process.env.PORT || 3000); 
 
 app.use(express.static(‘.’)
 
