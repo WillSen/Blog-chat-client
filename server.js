968aedc01d2d2b42653a82cc8bb0425b06e6bd86
Will Sentance <william.sentance@gmail.com>
2015-01-13 18:06:12 -0800

    fixes server code typos

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express'),
+    app = express(),
     server = require('http').Server(app),
     io = require('socket.io')(server)
 
 server.listen(process.env.PORT || 3000); 
 
-app.use(express.static('.')
+app.use(express.static('.'));
 
-io.on('connection', function(socket) {}
+io.on('connection', function(socket) {});
